25f34b77191623a7f7d3888b1a81e05280946b6e
Andres Arbona <68315861+AndresArbona@users.noreply.github.com>
2025-09-08 18:39:12 -0300

    VS 2022 setup and Main classes

---
+++ Source/AndresArbona_Task/Animator/PlayerAnimator.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "PlayerAnimator.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ANDRESARBONA_TASK_API UPlayerAnimator : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
