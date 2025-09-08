e72f2b0801915f21af80e210bbab852d598f6b90
Andres Arbona <68315861+AndresArbona@users.noreply.github.com>
2025-09-08 20:10:57 -0300

    Tag helpers and Enum of Inputs

--- Source/AndresArbona_Task/GAS/Enum/ESkateboardInputs.h
+++ Source/AndresArbona_Task/GAS/Enum/ESkateboardInputs.h
@@ -6,9 +6,15 @@
 #include "Engine/UserDefinedEnum.h"
 #include "ESkateboardInputs.generated.h"
 
-/**
- * 
- */
+UENUM(blueprintType)
+enum class EStakeboardInputID : uint8
+{
+	None UMETA(DisplayName = "None"),
+	Push UMETA(DisplayName = "Push"),
+	Brake UMETA(DisplayName = "Brake"),
+	Jump UMETA(DisplayName = "Jump")
+};
+
 UCLASS()
 class ANDRESARBONA_TASK_API UESkateboardInputs : public UUserDefinedEnum
 {
--- Source/AndresArbona_Task/GAS/SkateboardGameplayTagsList.h
+++ Source/AndresArbona_Task/GAS/SkateboardGameplayTagsList.h
@@ -2,16 +2,24 @@
 
 #pragma once
 
-#include "CoreMinimal.h"
-#include "GameplayTagsSettings.h"
-#include "SkateboardGameplayTagsList.generated.h"
+#include "GameplayTagContainer.h"
 
-/**
- * 
- */
-UCLASS()
-class ANDRESARBONA_TASK_API USkateboardGameplayTagsList : public UGameplayTagsList
+namespace SkateTags
 {
-	GENERATED_BODY()
-	
+	inline FGameplayTag State_Grounded() { return FGameplayTag::RequestGameplayTag(FName("State.Movement.Grounded")); }
+	inline FGameplayTag State_Grounded() { return FGameplayTag::RequestGameplayTag(FName("State.Movement.Airbone")); }
+	inline FGameplayTag State_Grounded() { return FGameplayTag::RequestGameplayTag(FName("State.Movement.Pushing")); }
+	inline FGameplayTag State_Grounded() { return FGameplayTag::RequestGameplayTag(FName("State.Movement.Braking")); }
+
+	inline FGameplayTag Cooldown_Push() { return FGameplayTag::RequestGameplayTag(FName("Cooldown.Push")); }
+
+	inline FGameplayTag Event_ObstableCleared() { return FGameplayTag::RequestGameplayTag(FName("Event.ObstacleCleared")); }
+	inline FGameplayTag Even_JumpCommited() { return FGameplayTag::RequestGameplayTag(FName("Cooldown.JumpCommited")); }
+	inline FGameplayTag Event_Landed() { return FGameplayTag::RequestGameplayTag(FName("Cooldown.Landed")); }
+
+	inline FGameplayTag Cue_Push() { return FGameplayTag::RequestGameplayTag(FName("GameplayCue.Push")); }
+	inline FGameplayTag Cue_Brake() { return FGameplayTag::RequestGameplayTag(FName("GameplayCue.Brake")); }
+	inline FGameplayTag Cue_Jump() { return FGameplayTag::RequestGameplayTag(FName("GameplayCue.Jump")); }
+	inline FGameplayTag Cue_Land() { return FGameplayTag::RequestGameplayTag(FName("GameplayCue.Land")); }
+	inline FGameplayTag Cue_Score() { return FGameplayTag::RequestGameplayTag(FName("GameplayCue.Score")); }
 };
